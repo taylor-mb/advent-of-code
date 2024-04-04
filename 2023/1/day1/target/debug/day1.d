1b100387b8d7d147b12a53e04ddaaa6d8bdc8714
Taylor Mendez Bauer <taylormendezbauer@gmail.com>
2024-04-03 21:31:43 -0500

    Remove build artifacts

--- 2023/1/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/taylorbauer/dev/advent-of-code/2023/1/day1/target/debug/day1: /Users/taylorbauer/dev/advent-of-code/2023/1/day1/src/main.rs
--- 2023/1/day1/target/debug/deps/day1-ce1e68060a5e9921.d
+++
@@ -1,5 +0,0 @@
-/Users/taylorbauer/dev/advent-of-code/2023/1/day1/target/debug/deps/libday1-ce1e68060a5e9921.rmeta: src/main.rs
-
-/Users/taylorbauer/dev/advent-of-code/2023/1/day1/target/debug/deps/day1-ce1e68060a5e9921.d: src/main.rs
-
-src/main.rs:
